70ecc7834c832eaa4798b892ef3d5dd3dc732d31
Sergio Siccha <sergio.siccha@gmail.com>
2020-03-25 14:53:40 +0100

    update generate-package-yml-files-... script

--- etc/generate-package-yml-files-from-PackageInfo-files.md
+++
@@ -1,8 +0,0 @@
-$GAPROOT should be the path to a gap installation
-From the root directory of the GapWWW repo:
-```
-mkdir _PackagesTest/tmp
-cp -r $GAPROOT/pkg _PackagesTest/tmp/
-find _PackagesTest/tmp/pkg/ -name 'PackageInfo.g' > _PackagesTest/tmp/list-of-paths-to-packageinfo-files.txt
-gap etc/generate-package-yml-files-from-PackageInfo-files.g
-```
--- etc/generate-package-yml-files-from-PackageInfo-files.sh
+++ etc/generate-package-yml-files-from-PackageInfo-files.sh
@@ -2,12 +2,15 @@
 collectionDir=_PackagesTest
 mkdir ${collectionDir}/tmp
 cd ${collectionDir}/tmp
+echo "Getting the GAP tarball.\n"
 wget https://www.gap-system.org/pub/gap/gap-4.11/tar.gz/gap-4.11.0.tar.gz
+echo "Unpacking the GAP tarball.\n"
 tar -xf gap-4.11.0.tar.gz
+echo "Generating YML files.\n"
 mv gap-*/pkg .
 rm -rf gap-*
 cd ../..
 find ${collectionDir}/tmp/pkg/ -name 'PackageInfo.g' > ${collectionDir}/tmp/list-of-paths-to-packageinfo-files.txt
-# use the gap version from the tar ball?
+# TODO use the gap version from the tar ball?
 gap etc/generate-package-yml-files-from-PackageInfo-files.g
 rm -rf ${collectionDir}/tmp
