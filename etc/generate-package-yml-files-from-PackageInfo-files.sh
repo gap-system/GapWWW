689975091858331973455e8502eb3454aa12bd06
Sergio Siccha <sergio.siccha@gmail.com>
2020-03-25 17:31:57 +0100

    update generate-package-yml-files-... script

--- etc/generate-package-yml-files-from-PackageInfo-files.sh
+++ etc/generate-package-yml-files-from-PackageInfo-files.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-# TODO change this to _Packages
-collectionDir=_PackagesTest
+# How to use this:
+# - Edit the gapTarballURL variable. Point it to the tarball of a gap release.
+# - run this script from the `GapWWW` repo root directory via
+#   `./etc/generate-package-yml-files-from-PackageInfo-files.sh`
+#
+# An executable `gap` must be in your $PATH, see the line before the last one
+# of this script.
+
+gapTarballURL='https://files.gap-system.org/gap-4.11/tar.gz/gap-4.11.0.tar.gz'
+collectionDir='_Packages'
 mkdir ${collectionDir}/tmp
 cd ${collectionDir}/tmp
 echo "----    Getting the GAP tarball    ----\n"
-# TODO put the correct link here
-wget https://files.gap-system.org/gap-4.11/tar.gz/gap-4.11.0.tar.gz
+wget ${gapTarballURL}
 echo "----    Unpacking the GAP tarball    ----\n"
 tar -xf gap-4.11.0.tar.gz
 echo "----    Generating YML files    ----\n"
