372dc13f998a4f0fb3a344f78c0227d555b43f18
Sergio Siccha <sergio.siccha@gmail.com>
2020-03-26 16:02:00 +0100

    update generate-package-yml-files-... script

--- etc/generate_package_yml_files_from_PackageInfo_files.sh
+++ etc/generate_package_yml_files_from_PackageInfo_files.sh
@@ -20,6 +20,8 @@ if [ -e ${pathToListOfPaths} ]; then
     exit 1
 fi
 
+# We use the maxdepth option since e.g. the hap package contains other gap
+# packages
 find ${1}/pkg/ -maxdepth 2 -name 'PackageInfo.g' > ${pathToListOfPaths}
 gap -q -c "path := \"${pathToListOfPaths}\";" etc/generate_package_yml_files_from_PackageInfo_files.g
 rm ${pathToListOfPaths}
