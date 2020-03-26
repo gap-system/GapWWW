6594f93e88eaace98dd9007734dd82a4ea71e65f
Max Horn <max@quendi.de>
2020-03-26 12:20:50 +0100

    Add data/help.yml and etc/LinksOfAllHelpSections.g

--- etc/sync_mixer_to_jekyll.py
+++ etc/sync_mixer_to_jekyll.py
@@ -7,29 +7,6 @@ import sys
 import os
 import re
 
-# sync lib/AllLinksOfAllHelpSections.data to _data/help.yml
-# TODO: in the future, this script here should be rendered unnecessary by
-# adapting the script which generates AllLinksOfAllHelpSections.data to
-# directly produce `_data/help.yml`
-input = 'lib/AllLinksOfAllHelpSections.data'
-output = '_data/help.yml'
-with open(input, "r") as instream, open(output, "w") as outstream:
-    outstream.write('---\n')
-    line = instream.readline()
-    while line:
-        # TODO: escape ' in the input
-        data = line.split("|")
-        if len(data) < 3:
-            line = line.rstrip() + instream.readline()
-            data = line.split("|")
-        
-        try:
-            outstream.write(repr(data[0]) + ": " + repr(data[1]) + "  # " + data[2])
-        except:
-            print("error on line: ", line)
-        line = instream.readline()
-        
-
 input = 'lib/config'
 output = '_data/gap.yml'
 with open(input, "r") as instream, open(output, "w") as outstream:
