219205e03134d30714da7bcac7a3033936cc8ce0
Max Horn <max@quendi.de>
2022-06-28 17:06:45 +0200

    Update etc/update.sh

--- etc/update.sh
+++ etc/update.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-jekyll build
+set -e
+
+# fetch latest changes
+cd $HOME/GapWWW
+git fetch --all --prune
+git checkout gh-pages
+git reset --hard origin/gh-pages
+
+# install prerequisites if necessary
+bundle config set --local path 'vendor/bundle'
+bundle install
+
+# run jekyll
+bundle exec jekyll build -d /srv/www/www.gap-system.org
